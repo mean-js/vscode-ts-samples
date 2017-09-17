68390793b64bc3e50c5d55efca1a7f8017265bd8
santosh <santosh.ece06@gmail.com>
2017-09-17 20:35:06 +0530

    object lterals

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import {first_method, first_method1} from "./core/first/first";
 import Student from "./core/second/Student";
 import {defaultArgumentTest, optionArugmentTest, tempFatRefObj} from "./core/second/temp";
-
+import obj_literal from "./core/second/temp_object_literal";
 
 class Main {
     public static main():void{
         try{
-            let sdata = tempFatRefObj.method4();
-            console.log(sdata);
+            
+            console.log(obj_literal);
+            console.log(obj_literal.getFirstName());
+
         }catch(err){
             console.error(err);
         }
---
+++ src/core/second/temp_object_literal.ts
@@ -0,0 +1,18 @@
+
+let title = "Affixus";
+let firstName = "Systems";
+let lastName = "Pvt. Ltd.";
+
+let obj_literal = {
+    "PI" : 3.14,
+    title,
+    firstName,
+    lastName,
+
+    getFirstName(){
+        return this.firstName;
+    }
+}
+
+
+export default obj_literal;
